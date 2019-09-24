e57d5028cda8c5c410e02a1e7e3c40fd81927aca
Alice <s10614619@gm.cyut.edu.tw>
2019-09-25 01:49:04 +0800

    顯示內容(未分頁)

--- controllers/MessageController.php
+++ controllers/MessageController.php
@@ -15,9 +15,23 @@ class MessageController extends Controller
     public function index()
     {
         $this->checkSignIn();
-        // $data = $this->model('message')->getMessage();
-        // $message_data = mysqli_fetch_assoc($data);
-        // $this->$smarty->assign("message_data", $message_data);
+        $data = $this->model('message')->getMessage();
+        $row_data = mysqli_fetch_assoc($data);
+        $message_data = [];
+        while ($row_data = mysqli_fetch_assoc($data)) {
+            $message_data[] = array(
+                "mb_no"          => $row_data["mb_no"],
+                "m_id"           => $row_data["m_id"],
+                "content"        => $row_data["content"],
+                "insert_time"    => $row_data["insert_time"],
+                "update_time"    => $row_data["update_time"],
+                "qualifications" => ($_SESSION['m_id'] === $row_data["m_id"] or $_SESSION['m_permission'] === '1') ? true : false,
+            );
+        }
+        $this->smarty->assign("title", "留言板");
+        $this->smarty->assign("m_id", $_SESSION["m_id"]);
+        $this->smarty->assign("m_no", $_SESSION["m_no"]);
+        $this->smarty->assign("message_data", $message_data);
         $this->smarty->display("message_select.html");
     }
 
